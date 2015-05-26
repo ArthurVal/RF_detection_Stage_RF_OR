7a6e4c8e58f76a835b1f0e3103a492b88d36f131
ArthurV <avalient@laas.fr>
2015-05-26 10:01:41 +0200

    Changing data format => Adding theta intensity map

--- src/RF_stub.cpp
+++ src/RF_stub.cpp
@@ -29,18 +29,6 @@ RF_stub::RF_stub(ros::Publisher* chatter_line_rviz, ros::Publisher* chatter_gaus
 /*------------------ Function that simulate the UART data input -------------------*/
 /*=================================================================================*/
 
-/*
-// Function to find the indice corresponding to the closest value of theta in tab 
-int getIndicePhi(double phi, double* tab, int size_tab){
-	for(int i = 0 ; i < size_tab ; ++i){		
-		if((*(tab+i)) < phi)
-			continue
-		else
-			return i;
-	}
-	return -1;	
-}
-*/
 
 void RF_stub::getDataUART()
 { 
@@ -50,16 +38,17 @@ void RF_stub::getDataUART()
 
 	data_uart_spherical_RF.n = nStub;
 
-	data_intensity_map_RF.index = iter;
+	data_intensity_map_RF_phi.index = data_intensity_map_RF_theta.index = iter;
 
 	for(int j = 0 ; j < SIZE_DATA_RF ; ++j)
-		data_intensity_map_RF.intensity[j] = 0;
+		data_intensity_map_RF_phi.intensity[j] = data_intensity_map_RF_theta.intensity[j] = 0;
 
 	for(int i = 0 ; i < (data_uart_spherical_RF.n) ; ++i){ 
 			//For all detection :
 
 		rStub = 2 - sin(ros::Time::now().toSec() + 2*i);
 		phiStub = 0 + 45 * cos(ros::Time::now().toSec() + 0.5*i);
+
 		if(phiStub < -180){
 			while(phiStub < -180){
 				phiStub += 360;
@@ -71,8 +60,21 @@ void RF_stub::getDataUART()
 			}
 		}
 
-		if(!thetaDisable)
+		if(!thetaDisable){
 			thetaStub = 90 + 45 * sin(ros::Time::now().toSec() + i);
+
+			if(thetaStub < 0){
+				while(thetaStub < 0){
+					thetaStub += 180;
+				}
+			}
+			if(thetaStub > 180){
+				while(thetaStub > 180){
+					thetaStub -= 180;
+				}
+			}
+		}
+	
 
 			//R
 		data_uart_spherical_RF.dist[i] = rStub;
@@ -85,8 +87,15 @@ void RF_stub::getDataUART()
 		double sigma = rStub/2; // Ecart type entre 0.5 et 1.5 degres
 
 		for(int j = 0 ; j < SIZE_DATA_RF ; ++j){
-			data_intensity_map_RF.intensity[j] = data_intensity_map_RF.intensity[j] + (1.0/(sigma * sqrt(2*M_PI))) *
-																																								exp(-0.5 * pow(((data_intensity_map_RF.phi[j]-phiStub) / sigma),2) );			
+			data_intensity_map_RF_phi.intensity[j] = data_intensity_map_RF_phi.intensity[j] + (1.0/(sigma * sqrt(2*M_PI))) *
+																																												exp(-0.5 * pow(((data_intensity_map_RF_phi.angle[j]-phiStub) / sigma),2) );			
 		}
+		for(int j = 0 ; j < (SIZE_DATA_RF/2) ; ++j){
+			data_intensity_map_RF_theta.intensity[j] = data_intensity_map_RF_theta.intensity[j] + (1.0/(sigma * sqrt(2*M_PI))) *
+																																												exp(-0.5 * pow(((data_intensity_map_RF_theta.angle[j]-thetaStub) / sigma),2) );			
+		}
+
+
+
 	}
 }
