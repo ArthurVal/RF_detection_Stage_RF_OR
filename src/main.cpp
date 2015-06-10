5bbd50d8a756d7e22e302983b8e75d2fdefed725
ArthurV <avalient@laas.fr>
2015-06-10 08:58:20 +0200

    Commit before pull

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@ void printHelp(){
 	std::cout << "\t --stub : enable stub mode (fake UART detection)" << std::endl;
 	std::cout << "\t --thetaDisable : Disable Theta for detection (2D detection R & Phi)" << std::endl;
 	std::cout << "\t --verbose : print data" << std::endl;
+	std::cout << "\t --remote : remote control mode : will publish data through rf_intensity_map_srv service" << std::endl;
 	std::cout << "\t --help : show this help" << std::endl;
 	std::cout << "====================================================================================" << std::endl;
 }
@@ -86,7 +87,7 @@ int main(int argc, char *argv[]){
 	ros::ServiceServer server_rf = r.advertiseService("rf_riddle_intensity_map_srv", &RF_detection::updateRF, detector);
 
 
-		//Loop rate frequency in Hz (correspond to the publishing frequency of the topic)
+		//Loop rate frequency in Hz (correspond to the publishing frequency of the topic) : should be lower (Acquisition of UART data is slower than 10 Hz and is function of Acquisition time variable)
 	ros::Rate loop_rate(10);
 
 	while(ros::ok()){	
